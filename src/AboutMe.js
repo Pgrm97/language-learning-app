ac4c8fea4cfe43655560c096d0c496cd84e43ad9
Pgrm97 <pgrm97@gmail.com>
2021-05-19 12:46:42 -0400

    Added button to change current country

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './style.css';
 import Countries from './countries.js';
 
+const { useState } = React;
+
 export default function AboutMe() {
+  const [SelectedCountry, setSelectedCountry] = useState('Dominican Republic');
+  const [SelectedCountryFlag, setSelectedCountryFlag] = useState();
   return (
     <div>
       <h1>Pedro Guillermo Rodríguez Menicucci</h1>
@@ -10,7 +14,15 @@ export default function AboutMe() {
         Born and raised in the city of Santo Domingo, Dominican Republic. A
         tropical island 🌴 located in the middle of the Caribbean Sea. 🌊
       </p>
-      <img src={Countries.Dominican} />
+      <p>Currently located in {SelectedCountry}</p>
+      <img src={Countries.Dominican.Flag} />
+      <button
+        onClick={() => {
+          setSelectedCountry('United Kingdom');
+        }}
+      >
+        {SelectedCountry}
+      </button>
     </div>
   );
 }
--- src/countries.js
+++ src/countries.js
@@ -1,4 +1,8 @@
 var Countries = {
-  Dominican:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_the_Dominican_Republic.svg/1280px-Flag_of_the_Dominican_Republic.svg.png'
+  Dominican: {
+    Flag:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_the_Dominican_Republic.svg/1280px-Flag_of_the_Dominican_Republic.svg.png'
+  }
 };
+
+export default Countries;
